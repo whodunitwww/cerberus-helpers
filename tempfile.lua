8fca90e4725a5abb02d038b7d406b59836d3e5df
whodunitwww <217437914+whodunitwww@users.noreply.github.com>
2026-08-15 16:14:02 +0800

    Update tempfile.lua

--- tempfile.lua
+++ tempfile.lua
@@ -257,13 +257,14 @@ local UNIVERSE_MAP = {
     ["10200395747"] = "gag2.lua", ["2644656496"] = "hazeSeas.lua", ["9199655655"] = "gakuran.lua",
     ["7613921865"] = "animeExpeditions.lua", ["4827308727"] = "havoc.lua", ["7395930870"] = "sellLemons.lua",
     ["10148749921"] = "animalHospital.lua", ["1511883870"] = "shindoLife.lua", ["7529591378"] = "practicalBasketball.lua", ["578392296"] = "aba.lua",
-    ["6035872082"] = "rivals.lua", ["6385532960"] = "ryujin.lua",
+    ["6035872082"] = "rivals.lua", ["6385532960"] = "ryujin.lua", ["10563114921"] = "stealAnEgg.lua"
 }
 
 local FREE_SCRIPTS = {
     ["animeExpeditions.lua"] = true, ["gag2.lua"] = true, ["slime.lua"] = true, ["deepwoken.lua"] = true,
     ["sellLemons.lua"] = true, ["animalHospital.lua"] = true, ["animeWarriors3.lua"] = true, ["bbn.lua"] = true,
     ["shindoLife.lua"] = true, ["practicalBasketball.lua"] = true, ["aba.lua"] = true, ["rivals.lua"] = true, ["ryujin.lua"] = true,
+    ["stealAnEgg.lua"] = true
 }
 
 local LUARMOR_SDK_URL   = "https://sdkapi-public.luarmor.net/library.lua"
@@ -890,4 +891,3 @@ task.spawn(function()
     task.wait(0.2)
     runScript(GITHUB_BASE_URL .. fileName, gameName, true)
 end)
-
