5db38ea3e26bd83bd389e2abf507fb8b639780f7
whodunitwww <217437914+whodunitwww@users.noreply.github.com>
2026-09-19 06:31:46 +0800

    Update tempfile.lua

--- tempfile.lua
+++ tempfile.lua
@@ -262,7 +262,8 @@ local UNIVERSE_MAP = {
     ["7613921865"] = "animeExpeditions.lua", ["4827308727"] = "havoc.lua", ["7395930870"] = "sellLemons.lua",
     ["10148749921"] = "animalHospital.lua", ["1511883870"] = "shindoLife.lua", ["7529591378"] = "practicalBasketball.lua", ["578392296"] = "aba.lua",
     ["6035872082"] = "rivals.lua", ["6385532960"] = "ryujin.lua", ["10563114921"] = "stealAnEgg.lua", ["4760747038"] = "wonderland.lua",
-    ["6739698191"] = "violenceDistrict.lua", ["9931749389"] = "dqr.lua", ["2459091562"] = "RH2.lua", ["7970033072"] = "theVeil.lua"
+    ["6739698191"] = "violenceDistrict.lua", ["9931749389"] = "dqr.lua", ["2459091562"] = "RH2.lua", ["7970033072"] = "theVeil.lua",
+    ["5595353122"] = "projectSlayers2.lua"
 }
 
 local FREE_SCRIPTS = {
@@ -270,7 +271,7 @@ local FREE_SCRIPTS = {
     ["sellLemons.lua"] = true, ["animalHospital.lua"] = true, ["animeWarriors3.lua"] = true, ["bbn.lua"] = true,
     ["shindoLife.lua"] = true, ["practicalBasketball.lua"] = true, ["aba.lua"] = true, ["rivals.lua"] = true, ["ryujin.lua"] = true,
     ["stealAnEgg.lua"] = true, ["wonderland.lua"] = true, ["violenceDistrict.lua"] = true, ["dqr.lua"] = true, ["RH2.lua"] = true,
-    ["bridger.lua"] = true, ["theVeil.lua"] = true
+    ["bridger.lua"] = true, ["theVeil.lua"] = true, ["projectSlayers2.lua"] = true
 }
 
 local LUARMOR_SDK_URL   = "https://sdkapi-public.luarmor.net/library.lua"
@@ -1026,4 +1027,3 @@ task.spawn(function()
     end
     boot()
 end)
-
